39db225814e6074e0dc59ecb998562022b48b7e9
SociableFish <jasonarokia@gmail.com>
2026-03-19 18:00:55 -0500

    Removed most of the app
    Will bring it back later

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,83 +1,14 @@
-import dataclasses
-from typing import Any, final
-
-import openai
 import PIL.Image
 import streamlit as st
-
-
-@final
-@dataclasses.dataclass(frozen=True, slots=True, kw_only=True)
-class Message:
-    role: Any
-    content: Any
-
-    def asdict(self, /) -> Any:
-        return {"role": self.role, "content": self.content}
-
-
-@final
-@dataclasses.dataclass(slots=True, kw_only=True)
-class SessionState:
-    client: openai.OpenAI | None
-    messages: list[Message]
-
-
-SYSTEM_MESSAGE: Message = Message(
-    role="system",
-    content=(
-        "You are a friendly math AI helper that helps school students get "
-        "better at math. Expected tasks include explaining solutions to math "
-        "problems, solving math problems, and explaining tricky math concepts."
-    )
-)
-
-
-DISPLAY_IF: frozenset[str] = frozenset(["user", "assistant"])
-
-
-TOKEN: str = st.secrets.TOKEN
 
 
 LOGO: PIL.Image.Image = PIL.Image.open("./JVA Logo.png")
 
 
-def session_state() -> SessionState:
-    """Gets a `SessionState` instance."""
-    if "state" not in st.session_state:
-        st.session_state.state = SessionState(
-            client=None,
-            messages=[SYSTEM_MESSAGE]
-        )
-    return st.session_state.state
-
-
 def main() -> None:
     st.image(LOGO)
-    st.title("JVA Math AI 0.1.0")
-    st.write(
-        "Are you unsatisfied with your math score? JVA Math AI can help you "
-        "raise it by analyzing your strengths and weaknesses, giving tailored "
-        "practice problems, and explaining solutions step-by-step."
-    )
-    state: SessionState = session_state()
-    client: openai.OpenAI = openai.OpenAI(api_key=TOKEN)
-    for message in state.messages:
-        if message.role not in DISPLAY_IF:
-            continue
-        with st.chat_message(message.role):
-            st.markdown(message.content)
-    if prompt := st.chat_input("What do you want help with?"):
-        state.messages.append(Message(role="user", content=prompt))
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            response = st.write_stream(client.chat.completions.create(
-                model="gpt-4",
-                messages=[m.asdict() for m in state.messages],
-                stream=True,
-            ))
-        state.messages.append(Message(role="assistant", content=response))
+    st.title("JVA Math 0.1.0")
+    st.text("This app is currently in progress. Come back later.")
 
 
 if __name__ == "__main__":
